c0abf173dcd206396f2ed9b3d115e563d672ab83
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-07-04 21:39:24 +0300

    fixed

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+namespace ExtraLongFactorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            extraLongFactorials(n);
+        }
+
+        private static void extraLongFactorials(int n)
+        {
+            BigInteger factorial = 1;
+            while (n > 0)
+            {
+                factorial *= n;
+                n--;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
